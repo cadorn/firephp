b1699b7831d832caa2d385b29c919697788f845c
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2007-04-01 03:19:46 +0000

    Tag /trunk/FirefoxExtension for release 0.0.5.2

--- apache/InfoTab.php
+++
@@ -1,2 +0,0 @@
-<p>Firebug Version: <script>document.write(FirePHPChannel.getFirePHPVersion());</script></p>
-<p>FirePHP Extension Version: <script>document.write(FirePHPChannel.getExtensionVersion());</script></p>
