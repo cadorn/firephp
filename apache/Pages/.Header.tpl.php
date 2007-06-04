5240ccd607b75ebaa562b6a253239ed150165c44
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2007-06-04 23:33:45 +0000

    - Added documentation to companion - Some minor tweaks to API core

--- apache/Pages/.Header.tpl.php
+++ apache/Pages/.Header.tpl.php
@@ -10,6 +10,7 @@
     }
   </style>
   <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <base href="<?php print substr($_SERVER['REQUEST_URI'],0,strpos($_SERVER['REQUEST_URI'],'/',1)+1); ?>"/>
 </head>
 
 <body topmargin="0" leftmargin="0" rightmargin="0" bottommargin="0" marginwidth="0" marginheight="0">
--- apache/Pages/Responses.php
+++ apache/Pages/Responses.php
@@ -1,8 +1,36 @@
 <?php include('../.Init.php');
       include('.Header.tpl.php'); ?>
 
-Responses
+<p><font size="2"><b>Cache Control Headers</b></font></p>
+<div style="padding-left: 10px;">
+<p>By default FirePHP sets some cache control headers in the response to disable caching of the response in proxies and your browser. This ensures your intelligence data is always up to date and your request is processed by the server each time. If this is not a desirable mode of operation for you, the cache control headers can be disabled. See the <a href="DefaultAPIWrapper.php">Default API Wrapper</a> Reference.</p>
+<p>The headers sent are:</p>
 
-- The API sets cache control headers for the response to time-out the page right-away. This may interfere with your application and caching logic and can be disabled. See the default API wrapper reference.
+<div style="margin: 10px; padding: 5px; padding-left: 15px; padding-right: 15px; background-color:#F9F9F9; overflow: auto;"><pre style="font-family: Courier;">
+<?php ob_start(); ?>
+Last-Modified: <?php print gmdate('r', time()); ?> 
+Expires: <?php print gmdate('r', time()-86400); ?> 
+Pragma: no-cache
+Cache-Control: no-cache, no-store, must-revalidate, max_age=0
+Cache-Control: post-check=0, pre-check=0 
+<?php print htmlentities(ob_get_clean()); ?></pre></div>
+</div>
+
+<p><font size="2"><b>FirePHP Headers</b></font></p>
+<div style="padding-left: 10px;">
+...
+</div>
+
+
+<p><font size="2"><b>Protocol Options</b></font></p>
+<div style="padding-left: 10px;">
+<p>FirePHP supports three different protocol options to send your intelligence data to the client. Each option has its pros and cons. By default the <i>Headers</i> option is used which simply sets the intelligence data as a response header. There are obvious limitations to this (such as requiring output buffering and limited size) which is why the other options are available.</p>
+</div>
+
+<p><font size="2" color="red"><b>Note:</b></font></p>
+<div style="padding-left: 10px;">
+<p>At this time only the <i><b>Header</b></i> option is supported.</p>
+</div>
+
 
 <?php include('.Footer.tpl.php'); ?>
