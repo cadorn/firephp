501fa17ec94b2e68850ac5dcb3eb0e0b5502842d
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2007-08-03 19:49:50 +0000

    Tag /trunk/PEARPackage for release 0.0.1.5

--- FirefoxExtension/defaults/preferences/firephp.js
+++
@@ -1,6 +0,0 @@
-
-// Menu Toggles
-pref("extensions.firephp.showRequestInspector", true);
-pref("extensions.firephp.showVariableInspector", true);
-pref("extensions.firephp.showConsole", true);
-pref("extensions.firephp.showApplicationToolbar", true);
