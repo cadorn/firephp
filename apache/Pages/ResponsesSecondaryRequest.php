4701f83dba08f8f901b83b397c4c828f56032ccd
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2007-11-22 09:03:02 +0000

    Tag /trunk/FirefoxExtension for release 0.0.5.21

--- apache/Pages/ResponsesSecondaryRequest.php
+++
@@ -1,9 +0,0 @@
-<?php include('../.Init.php');
-      include('.Header.tpl.php'); ?>
-
-<p><font size="2" color="red"><b>Note:</b></font></p>
-<div style="padding-left: 10px;">
-<p>At this time only the <i><b>Header</b></i> option is supported.</p>
-</div>
-
-<?php include('.Footer.tpl.php'); ?>
