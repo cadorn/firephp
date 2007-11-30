a7162ebd74a0027c628cf677c4f5b7bf1dba550a
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2007-11-30 19:31:49 +0000

    - Added TODO list

--- trunk/apache/Version1.js
+++ trunk/apache/Version1.js
@@ -1,3 +1,9 @@
 
 
-html = '<iframe width="100%" height="200" src="http://www.google.com/"></iframe>';
+
+html = data;
+
+Firebug.Console.log(data);
+
+
+//html = '<iframe width="100%" height="200" src="http://www.google.com/"></iframe>';
