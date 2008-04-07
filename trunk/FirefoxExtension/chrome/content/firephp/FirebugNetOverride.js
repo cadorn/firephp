d4d5874ec370e87337a4930e42fbb47e47e2466d
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-04-07 19:22:27 +0000

    - Added support for Firebug 1.1

--- trunk/FirefoxExtension/chrome/content/firephp/FirebugNetOverride.js
+++ trunk/FirefoxExtension/chrome/content/firephp/FirebugNetOverride.js
@@ -39,18 +39,200 @@
 
 FBL.ns(function() { with (FBL) {
 
-
-/* Only override the net code for firebug version 1.01 */
-
-if(Firebug.version=='1.05') {
-	
-			
 const binaryCategoryMap =
 {
     "image": 1,
     "flash" : 1
 };
+
+
+//dump('Firebug.version: '+Firebug.version+"\n");
+
+
+/* Only override the net code for specific firebug versions */
+
+if(Firebug.version=='1.1') {		/* 1.1.0b12 */
+
+
+Firebug.NetMonitor.NetInfoBody = domplate(Firebug.NetMonitor.NetInfoBody,
+{
+    tag:
+        DIV({class: "netInfoBody", _repObject: "$file"},
+            DIV({class: "netInfoTabs"},
+                A({class: "netInfoParamsTab netInfoTab", onclick: "$onClickTab",
+                    view: "Params",
+                    $collapsed: "$file|hideParams"},
+                    $STR("URLParameters")
+                ),
+                A({class: "netInfoHeadersTab netInfoTab", onclick: "$onClickTab",
+                    view: "Headers"},
+                    $STR("Headers")
+                ),
+                A({class: "netInfoPostTab netInfoTab", onclick: "$onClickTab",
+                    view: "Post",
+                    $collapsed: "$file|hidePost"},
+                    $STR("Post")
+                ),
+                A({class: "netInfoResponseTab netInfoTab", onclick: "$onClickTab",
+                    view: "Response",
+                    $collapsed: "$file|hideResponse"},
+                    $STR("Response")
+                ),
+                A({class: "netInfoCacheTab netInfoTab", onclick: "$onClickTab",
+                   view: "Cache",
+                   $collapsed: "$file|hideCache"},
+                   "Cache" // todo: Localization
+                ),
+                A({class: "netInfoServerTab netInfoTab", onclick: "$onClickTab",
+                    view: "Server",
+                    $collapsed: "$file|hideServer"},
+                    "Server"
+                )
+            ),
+            TABLE({class: "netInfoParamsText netInfoText netInfoParamsTable",
+                    cellpadding: 0, cellspacing: 0}, TBODY()),
+            TABLE({class: "netInfoHeadersText netInfoText netInfoHeadersTable",
+                    cellpadding: 0, cellspacing: 0},
+                TBODY(
+                    TR({class: "netInfoResponseHeadersTitle"},
+                        TD({colspan: 2},
+                            DIV({class: "netInfoHeadersGroup"}, $STR("ResponseHeaders"))
+                        )
+                    ),
+                    TR({class: "netInfoRequestHeadersTitle"},
+                        TD({colspan: 2},
+                            DIV({class: "netInfoHeadersGroup"}, $STR("RequestHeaders"))
+                        )
+                    )
+                )
+            ),
+            DIV({class: "netInfoPostText netInfoText"},
+                TABLE({class: "netInfoPostTable", cellpadding: 0, cellspacing: 0},
+                    TBODY()
+                )
+            ),
+            DIV({class: "netInfoResponseText netInfoText"},
+                $STR("Loading")
+            ),
+            DIV({class: "netInfoCacheText netInfoText"},
+                TABLE({class: "netInfoCacheTable", cellpadding: 0, cellspacing: 0},
+                    TBODY()
+                )
+            ),
+            DIV({class: "netInfoServerText netInfoText"}, 
+                $STR("Loading")
+            ) 
+        ),
+
+
+    updateInfo: function(netInfoBox, file, context)
+    {
+        var tab = netInfoBox.selectedTab;
+        if (hasClass(tab, "netInfoParamsTab"))
+        {
+            if (file.urlParams && !netInfoBox.urlParamsPresented)
+            {
+                netInfoBox.urlParamsPresented = true;
+                this.insertHeaderRows(netInfoBox, file.urlParams, "Params");
+            }
+        }
+
+        if (hasClass(tab, "netInfoHeadersTab"))
+        {
+            if (file.responseHeaders && !netInfoBox.responseHeadersPresented)
+            {
+                netInfoBox.responseHeadersPresented = true;
+                this.insertHeaderRows(netInfoBox, file.responseHeaders, "Headers", "ResponseHeaders");
+            }
+
+            if (file.requestHeaders && !netInfoBox.requestHeadersPresented)
+            {
+                netInfoBox.requestHeadersPresented = true;
+                this.insertHeaderRows(netInfoBox, file.requestHeaders, "Headers", "RequestHeaders");
+            }
+        }
+
+        if (hasClass(tab, "netInfoPostTab"))
+        {
+            var postTextBox = getChildByClass(netInfoBox, "netInfoPostText");
+            if (!netInfoBox.postPresented)
+            {
+                netInfoBox.postPresented  = true;
+
+                var text = getPostText(file, context);
+                if (text != undefined)
+                {
+                    if (isURLEncodedFile(file, text))
+                    {
+                        var lines = text.split("\n");
+                        var params = parseURLEncodedText(lines[lines.length-1]);
+                        this.insertHeaderRows(netInfoBox, params, "Post");
+                    }
+                    else
+                    {
+                        var postText = formatPostText(text);
+                        if (postText)
+                            insertWrappedText(postText, postTextBox);
+                    }
+                }
+            }
+        }
+
+        if (hasClass(tab, "netInfoResponseTab") && file.loaded && !netInfoBox.responsePresented)
+        {
+            netInfoBox.responsePresented = true;
+
+            var responseTextBox = getChildByClass(netInfoBox, "netInfoResponseText");
+            if (file.category == "image")
+            {
+                var responseImage = netInfoBox.ownerDocument.createElement("img");
+                responseImage.src = file.href;
+                responseTextBox.replaceChild(responseImage, responseTextBox.firstChild);
+            }
+            else if (!(file.category in binaryCategoryMap))
+            {
+                var text = file.responseText
+                    ? file.responseText
+                    : context.sourceCache.loadText(file.href);
+
+                if (text)
+                    insertWrappedText(text, responseTextBox);
+                else
+                    insertWrappedText("", responseTextBox);
+            }
+        }
+
+        if (hasClass(tab, "netInfoCacheTab") && file.loaded && !netInfoBox.cachePresented)
+        {
+            netInfoBox.cachePresented = true;
+
+            var responseTextBox = getChildByClass(netInfoBox, "netInfoCacheText");
+            if(file.cacheEntry) {
+              this.insertHeaderRows(netInfoBox, file.cacheEntry, "Cache");
+            }
+        }
+				
+        if (hasClass(tab, "netInfoServerTab") && file.loaded && !netInfoBox.serverPresented)
+        {
+					netInfoServerTab(netInfoBox, file, context);
+        }				
+				
+    },
 		
+
+    hideServer: function(file)
+    {
+        return false;
+    }	
+		
+});
+
+
+
+} else
+if(Firebug.version=='1.05') {
+	
+			
 		
 Firebug.NetMonitor.NetInfoBody = domplate(Firebug.NetMonitor.NetInfoBody,
 {
@@ -78,7 +260,7 @@ Firebug.NetMonitor.NetInfoBody = domplate(Firebug.NetMonitor.NetInfoBody,
                 ),
                 A({class: "netInfoServerTab netInfoTab", onclick: "$onClickTab",
                     view: "Server",
-                    $collapsed: "$file|hideResponse"},
+                    $collapsed: "$file|hideServer"},
                     "Server"
                 )
             ),
@@ -188,6 +370,55 @@ Firebug.NetMonitor.NetInfoBody = domplate(Firebug.NetMonitor.NetInfoBody,
 				
         if (hasClass(tab, "netInfoServerTab") && file.loaded && !netInfoBox.serverPresented)
         {
+					netInfoServerTab(netInfoBox, file, context);
+        }				
+    },
+		
+    hideServer: function(file)
+    {
+        return false;
+    }		
+});
+
+
+}
+
+
+
+function insertWrappedText(text, textBox)
+{
+    var reNonAlphaNumeric = /[^A-Za-z_$0-9'"-]/;
+
+    var html = [];
+    var wrapWidth = Firebug.textWrapWidth;
+        
+    var lines = splitLines(text);
+    for (var i = 0; i < lines.length; ++i)
+    {
+        var line = lines[i];
+        while (line.length > wrapWidth)
+        {
+            var m = reNonAlphaNumeric.exec(line.substr(wrapWidth, 100));
+            var wrapIndex = wrapWidth+ (m ? m.index : 0);
+            var subLine = line.substr(0, wrapIndex);
+            line = line.substr(wrapIndex);
+
+            html.push("<pre>");
+            html.push(escapeHTML(subLine));
+            html.push("</pre>");
+        }
+
+        html.push("<pre>");
+        html.push(escapeHTML(line));
+        html.push("</pre>");
+    }
+
+    textBox.innerHTML = html.join("");    
+}
+
+
+function netInfoServerTab(netInfoBox, file, context) {
+	
             netInfoBox.serverPresented = true;
 
             var responseTextBox = getChildByClass(netInfoBox, "netInfoServerText");
@@ -259,10 +490,9 @@ Firebug.NetMonitor.NetInfoBody = domplate(Firebug.NetMonitor.NetInfoBody,
 																						'You can view the License at <a target="_blank" href="http://www.mozilla.org/MPL/">http://www.mozilla.org/MPL/</a>.</p>';
 																						
 							}
-						}
-        }				
-    }	
-});
+						}	
+}
+
 
 
 function parseAndPrintData(Data, Mask, responseTextBox,doc,hash) {
@@ -376,39 +606,6 @@ function parseAndPrintData(Data, Mask, responseTextBox,doc,hash) {
 }
 
 
-
-function insertWrappedText(text, textBox)
-{
-    var reNonAlphaNumeric = /[^A-Za-z_$0-9'"-]/;
-
-    var html = [];
-    var wrapWidth = Firebug.textWrapWidth;
-        
-    var lines = splitLines(text);
-    for (var i = 0; i < lines.length; ++i)
-    {
-        var line = lines[i];
-        while (line.length > wrapWidth)
-        {
-            var m = reNonAlphaNumeric.exec(line.substr(wrapWidth, 100));
-            var wrapIndex = wrapWidth+ (m ? m.index : 0);
-            var subLine = line.substr(0, wrapIndex);
-            line = line.substr(wrapIndex);
-
-            html.push("<pre>");
-            html.push(escapeHTML(subLine));
-            html.push("</pre>");
-        }
-
-        html.push("<pre>");
-        html.push(escapeHTML(line));
-        html.push("</pre>");
-    }
-
-    textBox.innerHTML = html.join("");    
-}
-
-}
 
 }});
 
