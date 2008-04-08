7e1054bb8144e5d23bca0af0f89f9c9a5c3ec570
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-04-08 01:06:19 +0000

    Tag /trunk/FirefoxExtension for release 0.0.6

---
+++ tags/FirefoxExtension-0.0.6/chrome/content/firephp/RequestProcessor.js
@@ -0,0 +1,30 @@
+
+/* 
+ * Called once for each request as it comes in
+ */
+FirePHPProcessor.ProcessRequest = function() {
+
+	if (this.data['FirePHP.Firebug.Console']) {
+  
+  	Firebug.Console.openGroup(this.url, null, "group", null, false);
+	
+		for (var index in this.data['FirePHP.Firebug.Console']) {
+			
+			var item = this.data['FirePHP.Firebug.Console'][index];
+			
+			if (item[0] == 'log' || item[0] == 'info' || item[0] == 'warn') {
+	  
+	  		this.logToFirebug(item[0], item[1]);
+	  	
+			} else 
+	  	if (item[0] == 'error') {
+	  		
+				Firebug.Errors.increaseCount(this.context);
+				
+	  		this.logToFirebug(item[0], item[1]);
+			}
+		}	
+	
+		Firebug.Console.closeGroup(this.context);
+  } 	
+}
