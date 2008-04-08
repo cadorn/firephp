8b635e22c79361f64d4f364e81cacac52a785eca
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-04-08 01:52:57 +0000

    Tag /trunk/FirefoxExtension for release 0.0.6.2

--- apache/Test1.php
+++
@@ -1,9 +0,0 @@
-
-  <script type="application/x-javascript" src="jquery.js"/>
-
-
-<iframe src="Version1.php?frame=1" width="500" height="300"></iframe>
-
-<iframe src="Version1.php?frame=2" width="500" height="300"></iframe>
-
-<p><a href="#" onClick="$.get('Version1.php?var=1');">Link</a></p>
