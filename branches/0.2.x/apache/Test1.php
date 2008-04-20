ea41a86a4ae8ed5069dc09a67839c4ad206b96bb
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-04-20 00:30:31 +0000

    Branch from 0.2.x development.

---
+++ branches/0.2.x/apache/Test1.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+  <script type="application/x-javascript" src="jquery.js"></script>
+</head>
+
+<body>
+  
+<iframe src="Version1.php?frame=1" width="500" height="300"></iframe>
+
+<iframe src="Version1.php?frame=2" width="500" height="300"></iframe>
+
+<p><a href="#" onClick="$.get('Version1.php?var=1');">Version1.php?var=1</a></p>
+<p><a href="#" onClick="$.get('ConsoleTest.php');">ConsoleTest.php</a></p>
+<p><a href="#" onClick="$.get('ZendTest.php');">ZendTest.php</a></p>
+
+</body>
+</html>
