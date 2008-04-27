b07ab31012efde653a8674b370b0777c24905b7c
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-04-27 19:33:31 +0000

    Renamed FirePHP Core library directory

--- trunk/Libraries/index.php
+++ trunk/Libraries/index.php
@@ -1,5 +1,5 @@
 
-<p><a href="Core/demo/procedural.php">Core (Procedural)</a></p>
-<p><a href="Core/demo/oo.php">Core (Object Oriented)</a></p>
+<p><a href="FirePHPCore/demo/procedural.php">Core (Procedural)</a></p>
+<p><a href="FirePHPCore/demo/oo.php">Core (Object Oriented)</a></p>
 
 <p><a href="ZendFramework/demo/public/">Zend Framework</a></p>
