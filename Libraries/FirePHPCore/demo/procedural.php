e6adfd6281000073984a5f7ea67b9510d042b414
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-06-07 03:58:22 +0000

    Updated demos for TABLE log style

--- Libraries/FirePHPCore/demo/procedural.php
+++ Libraries/FirePHPCore/demo/procedural.php
@@ -4,7 +4,7 @@
  *  
  * This file is part of FirePHP (http://www.firephp.org/).
  * 
- * Copyright (C) 2007 Christoph Dorn
+ * Copyright (C) 2007-2008 Christoph Dorn
  * 
  * FirePHP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -55,7 +55,15 @@ try {
   fb($e);
 }
 
+fb(array('2 SQL queries took 0.06 seconds',array(
+   array('SQL Statement','Time','Result'),
+   array('SELECT * FROM Foo','0.02',array('row1','row2')),
+   array('SELECT * FROM Bar','0.04',array('row1','row2'))
+  )),FirePHP::TABLE);
+
 /* Will show only in "Server" tab for the request */
 fb(apache_request_headers(),'RequestHeaders',FirePHP::DUMP);
 
-?>
+
+print 'Hello World';
+
