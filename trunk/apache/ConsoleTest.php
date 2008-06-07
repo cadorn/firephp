33bd218177b0f4f53b460a6749f0864de5cd7e3d
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-06-07 03:51:38 +0000

    Added support for FirePHP::TABLE log style

--- trunk/apache/ConsoleTest.php
+++ trunk/apache/ConsoleTest.php
@@ -55,14 +55,27 @@ fb('Hello World');
 fb('<a onclick="alert(\'Hello World\');">ClickMe!</a>');
 
 
-fb('Log message',FB_LOG);
-fb('Info message',FB_INFO);
-fb('Warn message',FB_WARN);
-fb('Error message',FB_ERROR);
 
-fb('Message with label','Label',FB_LOG);
+$log = array();
+$log[] = array('SQL Statement','Time','Result');
+$log[] = array('SELECT * FROM Foo','0.02',array('row1','row2'));
+$log[] = array('SELECT * FROM Bar','0.04',array('row1','row2'));
 
-fb(array('key1'=>'val1','key2'=>array(array('v1','v2'),'v3')),'TestArray',FB_LOG);
+fb(array('2 SQL queries took 0.06 seconds',$log),FirePHP::TABLE);
+
+
+
+
+
+
+fb('Log message',FirePHP::LOG);
+fb('Info message',FirePHP::INFO);
+fb('Warn message',FirePHP::WARN);
+fb('Error message',FirePHP::ERROR);
+
+fb('Message with label','Label',FirePHP::LOG);
+
+fb(array('key1'=>'val1','key2'=>array(array('v1','v2'),'v3')),'TestArray',FirePHP::LOG);
 
 function test($Arg1) {
   throw new Exception('Test Exception');
@@ -74,7 +87,7 @@ try {
 }
 
 
-fb(apache_request_headers(),'RequestHeaders',FB_DUMP);
+fb(apache_request_headers(),'RequestHeaders',FirePHP::DUMP);
 
 
 //header('X-FirePHP-ProcessorURL: http://'.$_SERVER['HTTP_HOST'].'/Version1-Processor.js');
---
+++ trunk/apache/NoContentTest.php
@@ -0,0 +1,9 @@
+<?php
+
+ob_start();
+
+require_once('./../Libraries/FirePHPCore/lib/FirePHPCore/fb.php');
+
+fb('Hello World');
+  
+
