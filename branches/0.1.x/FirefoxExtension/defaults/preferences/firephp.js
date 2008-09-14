ed2f7d58e7df73aefee739c4e87db26c2d96e18e
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-09-14 03:48:08 +0000

    Branch reorganization

--- branches/0.1.x/FirefoxExtension/defaults/preferences/firephp.js
+++
@@ -1,5 +0,0 @@
-
-// Global
-pref("extensions.firephp.enabled", true);
-pref("extensions.firephp.previousVersion", '');
-pref("extensions.firephp.currentVersion", '');
--- branches/0.1.x/Libraries/ZendFramework2/tests/TestConfiguration.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * PHPUnit Code Coverage / Test Report
- */ 
-define('TESTS_GENERATE_REPORT', false);
-define('TESTS_GENERATE_REPORT_TARGET', '/path/to/report'); 
-
