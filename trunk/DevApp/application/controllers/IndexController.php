e69835446a4fb57f5a1282a67f1ccc8067703c91
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-09-19 21:36:18 +0000

    Updated Dev app to include basic tests for 0.1 and 0.2 branches of the FirePHPCore library

--- trunk/DevApp/application/controllers/IndexController.php
+++ trunk/DevApp/application/controllers/IndexController.php
@@ -6,7 +6,17 @@ class IndexController extends Zend_Controller_Action
       
     public function indexAction()
     {
+
+      if($header = $this->getRequest()->getHeader('User-Agent')) {
+        preg_match_all('/\s?FirePHP\/([\.|\d]*)\s?/si',$header,$m);
+        if(isset($m[1][0])) {
+            $this->view->FirePHPUserAgentVersion = $m[1][0];
+        }
+      } else {
+        $this->view->FirePHPUserAgentVersion = false;        
+      }
       
+
 
 //        $test = new Test();
 //        $test->testModelLogging();      
--- trunk/DevApp/public/plain.php
+++ trunk/DevApp/public/plain.php
@@ -1,7 +1,7 @@
 <?php
 
 $file = dirname(dirname(__FILE__)) . '/application/bootstrap/plain/'
-                                   . basename($_SERVER['REQUEST_URI']);
+                                   . basename($_SERVER['REDIRECT_URL']);
 
 if(!file_exists($file)) {
   die('File not found!');
