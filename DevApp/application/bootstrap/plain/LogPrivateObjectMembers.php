8d561b2b5779db9d8090c9765fec0208f4f63eae
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-10-05 11:24:41 +0000

    Add random test file Updated private/protected variable dumping test

--- DevApp/application/bootstrap/plain/LogPrivateObjectMembers.php
+++ DevApp/application/bootstrap/plain/LogPrivateObjectMembers.php
@@ -1,19 +1,38 @@
 <?php
 
-class Dumper
+class FirePHP_VariableEncoder
 {
-  protected $_properties = array();
-  
-  protected function _setState($class, $members) {
+  public static function encode($variable)
+  {
+    $code = var_export($variable, true);
+    
+    if(preg_match_all('/[\s>=](\S*?)::__set_state\(/si',$code,$m)) {
+      for( $i=0; $i < count($m[0]); $i++ ) {
+        $code = preg_replace('/'.preg_quote($m[0][$i],'/').'/',
+                                'FirePHP_VariableEncoder::__set_state(\''.$m[1][$i].'\',',
+                                $code);
+    	}
+    }
+    
+    eval('$dump = ' . $code . ';');
+    
+    return $dump;
+  }
 
+  public static function __set_state($class, $members)
+  {    
     $reflection_class = new ReflectionClass($class);  
     
-    $this->_setProperties($reflection_class->getProperties());
+    $props = array();
+    foreach( $reflection_class->getProperties() as $property) {
+      $props[$property->getName()] = $property;
+    }
   
     $dump = array('__className'=>$class);
 
-    foreach( $this->_properties as $name => $property ) {
+    foreach( $props as $raw_name => $property ) {
 
+      $name = $raw_name;
       if($property->isStatic()) {
         $name = 'static:'.$name;
       }
@@ -28,13 +47,13 @@ class Dumper
         $name = 'protected:'.$name;
       }
       
-      if($members[$name]) {
-        $dump[$name] = $members[$name];      
+      if($members[$raw_name]) {
+        $dump[$name] = $members[$raw_name];      
       } else {
 
         if(method_exists($property,'setAccessible')) {
-//        $property->setAccessible(true);
-//        $dump[$name] = $property->getValue();
+          $property->setAccessible(true);
+          $dump[$name] = $property->getValue();
         } else {
           $dump[$name] = 'Need PHP 5.3 to get value!';
         }
@@ -42,36 +61,18 @@ class Dumper
     }    
     
     foreach( $members as $name => $value ) {
-      
-      if($this->_properties[$name]->isPublic()) {
-        $name = 'public:'.$name;
-      } else
-      if($this->_properties[$name]->isPrivate()) {
-        $name = 'private:'.$name;
-      } else
-      if($this->_properties[$name]->isProtected()) {
-        $name = 'protected:'.$name;
+      // Include all members that are not defined in the class
+      // but exist in the object
+      if(!$props[$name]) {
+        $name = 'undeclared:'.$name;
+        $dump[$name] = $value;
       }
-      
-      $dump[$name] = $value;
     }
     return $dump;
   }
-  
-  public static function __set_state($class, $members)
-  {    
-    $dumper = new self();
-    return $dumper->_setState($class,$members);
-  }
-  
-  protected function _setProperties($Properties)
-  {
-    foreach( $Properties as $property) {
-      $this->_properties[$property->getName()] = $property;
-    }
-  }
-  
 }
+
+
 
 class TestObject
 {
@@ -80,22 +81,28 @@ class TestObject
   private $privateVar = 'PrivateVar';
 }
 
+
+class TestObject2
+{
+  var $publicVar = 'Public Var';
+  private $privateVar = 'PrivateVar';
+}
+
+
 $obj = new TestObject();
 
-$code = var_export($obj, true);
+$obj2 = new TestObject2();
+
+$obj->child = $obj2;
+
+$obj = array('hello'=>'world','obj'=>$obj,'last'=>30,array('foo'=>'bar'),array('first','second'));
+
 
 echo '<pre>';
-echo $code;
+var_dump($obj);
 echo '</pre>';
 
-$code = str_replace('TestObject::__set_state(',
-                    'Dumper::__set_state(\'TestObject\',', $code);
-
-echo '<pre>';
-echo $code;
-echo '</pre>';
-
-eval('$dump = ' . $code . ';');
+$dump = FirePHP_VariableEncoder::encode($obj);
 
 echo '<pre>';
 var_dump($dump);
