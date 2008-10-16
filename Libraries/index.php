9d567c84a92ed14e61db3539dec6bcb9c8c37256
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-10-16 20:04:57 +0000

    Tag /branches/FirefoxExtension-0.2/FirefoxExtension for release 0.2.b.4

--- Libraries/index.php
+++
@@ -1,7 +0,0 @@
-
-<p><a href="FirePHPCore/demo/procedural.php">Core (Procedural)</a></p>
-<p><a href="FirePHPCore/demo/oo.php">Core (Object Oriented)</a></p>
-
-<p><a href="ZendFramework/demo/public/">Zend Framework (Old)</a></p>
-
-<p><a href="ZendFramework2/demo/public/">Zend Framework (New)</a></p>
