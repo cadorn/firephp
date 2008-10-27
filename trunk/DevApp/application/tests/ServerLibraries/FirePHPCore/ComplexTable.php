1c9ce0b4fd301e0e17e60dd239dd7fe8309f159a
christoph.dorn <christoph.dorn@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2008-10-27 02:29:05 +0000

    Updates to tests

--- trunk/DevApp/application/tests/ServerLibraries/FirePHPCore/ComplexTable.php
+++ trunk/DevApp/application/tests/ServerLibraries/FirePHPCore/ComplexTable.php
@@ -12,7 +12,7 @@ $table[] = array('Row 2 Col 1', 'Row 2 Col 2', 'Row 2 Col3');
 
 $row = array();
 $row[] = '<p>Value for column 1</p>';
-$row[] = 'This is a very long value for column 2. kjhsdgf ksd sadkfhgsadhfs adfjhksagdfkhjsadgf sakjhdfgasdhkfgsjhakdf jkhsadfggksadfg iweafiuwaehfiulawhef liawefiluhawefiuhwaeiufl iulhaweiuflhwailuefh iluwahefiluawhefuiawefh lwaieufhwaiulefhawef liawuefhawiluefhawfl';
+$row[] = 'This is a very long value for column 2.'."\n\n".' kjhsdgf ksd sadkfhgsadhfs adfjhksagdfkhjsadgf sakjhdfgasdhkfgsjhakdf jkhsadfggksadfg iweafiuwaehfiulawhef liawefiluhawefiuhwaeiufl iulhaweiuflhwailuefh iluwahefiluawhefuiawefh lwaieufhwaiulefhawef liawuefhawiluefhawfl';
 $row[] = '<p>First paragraph</p>'."\n".'<p>Second paragraph</p>';
 $table[] = $row;
 
