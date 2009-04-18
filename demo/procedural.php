a6c5757b3a38dfe9f2724fda2b9da7e1a81d280a
christoph@christophdorn.com <christoph@christophdorn.com@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2009-04-18 04:07:58 +0000

    - Renaming branch to get it out of the way of a new branching convention

---
+++ demo/procedural.php
@@ -0,0 +1,61 @@
+<?php
+
+/* ***** BEGIN LICENSE BLOCK *****
+ *  
+ * This file is part of FirePHP (http://www.firephp.org/).
+ * 
+ * Copyright (C) 2007 Christoph Dorn
+ * 
+ * FirePHP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * FirePHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FirePHP.  If not, see <http://www.gnu.org/licenses/lgpl.html>.
+ * 
+ * ***** END LICENSE BLOCK ***** */
+
+
+/* NOTE: You must have the FirePHPCore library in your include path */
+
+set_include_path('./../lib/'.PATH_SEPARATOR.get_include_path());
+ 
+
+require('FirePHPCore/fb.php');
+
+/* NOTE: You must have Output Buffering enabled via
+         ob_start() or output_buffering ini directive. */
+
+fb('Hello World'); /* Defaults to FirePHP::LOG */
+
+fb('Log message'  ,FirePHP::LOG);
+fb('Info message' ,FirePHP::INFO);
+fb('Warn message' ,FirePHP::WARN);
+fb('Error message',FirePHP::ERROR);
+
+fb('Message with label','Label',FirePHP::LOG);
+
+fb(array('key1'=>'val1',
+         'key2'=>array(array('v1','v2'),'v3')),
+   'TestArray',FirePHP::LOG);
+
+function test($Arg1) {
+  throw new Exception('Test Exception');
+}
+try {
+  test(array('Hello'=>'World'));
+} catch(Exception $e) {
+  /* Log exception including stack trace & variables */
+  fb($e);
+}
+
+/* Will show only in "Server" tab for the request */
+fb(apache_request_headers(),'RequestHeaders',FirePHP::DUMP);
+
+?>
