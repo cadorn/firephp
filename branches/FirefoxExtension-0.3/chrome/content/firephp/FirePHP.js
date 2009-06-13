c57f025c943d2ab970c1d7dc579ed95e0e5ffe0f
christoph@christophdorn.com <christoph@christophdorn.com@ee213dd3-8126-0410-934e-dd8c6ffc1f94>
2009-06-13 19:23:46 +0000

    (Issue 110) No console logging in iceweasel 3.0.6 with firebug 1.3.3.

--- branches/FirefoxExtension-0.3/chrome/content/firephp/FirePHP.js
+++ branches/FirefoxExtension-0.3/chrome/content/firephp/FirePHP.js
@@ -182,14 +182,9 @@ var FirePHP = top.FirePHP = {
 
       /* Add FirePHP/X.X.X to User-Agent header if not already there and firephp is enabled.
        * If firephp is not enabled remove header from request if it exists.
-       * 
-       * (Issue 104)
-       * Only add FirePHP header if useragent contains Firefox. If not the user-agent string
-       * was modified by an add-on on purpose (like User Agent Switcher) and FirePHP should not be added.
        */
 
-      if(httpChannel.getRequestHeader("User-Agent").match(/\sFirePHP\/([\.|\d]*)\s?/)==null &&
-         httpChannel.getRequestHeader("User-Agent").match(/\sFirefox\//)!=null) {
+      if(httpChannel.getRequestHeader("User-Agent").match(/\sFirePHP\/([\.|\d]*)\s?/)==null) {
         if (this.isEnabled()) {
           httpChannel.setRequestHeader("User-Agent",httpChannel.getRequestHeader("User-Agent") + ' '+
             "FirePHP/" + this.version, false);
